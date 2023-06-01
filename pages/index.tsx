47ae20478e427554ef8d16ff904752f50d87af99
joenzkc <jokichaa@gmail.com>
2023-06-01 16:30:45 +0800

    fix min w issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Link from "next/link";
 import React from "react";
 export default function Home() {
   const [shortenedUrl, setShortenedUrl] = React.useState("");
@@ -47,32 +48,33 @@ export default function Home() {
   }
 
   return (
-    <div className="mx-auto bg-slate-700 w-full">
-      <div className="mx-auto w-6/7 max-w-2xl pb-3 pt-20">
-        <h1 className="text-center font-bold font-montserrat text-white text-6xl py-10">
-          URL Shortener
-        </h1>
-      </div>
-      <div className="mx-auto">
-        <p className="text-center text-lg text-slate-400 pb-2 font-montserrat">
-          Paste the URL to be shortened here:
-        </p>
-      </div>
-      <div className="mx-auto max-w-xl px-4 pb-4">
-        <form
-          method="post"
-          className="flex flex-1 justify-center align-middle"
-          onSubmit={handleSubmit}
-        >
-          <input
-            className={isValid ? validInputForm : invalidInputForm}
-            placeholder="Enter link here"
-            onChange={(e) => {
-              setUrl(e.target.value);
-            }}
-          ></input>
-          <button
-            className="border-2
+    <div>
+      <div className="mx-auto bg-slate-700 w-full">
+        <div className="mx-auto w-6/7 max-w-2xl pb-3 pt-20">
+          <h1 className="text-center font-bold font-montserrat text-white text-6xl py-10">
+            URL Shortener
+          </h1>
+        </div>
+        <div className="mx-auto">
+          <p className="text-center text-lg text-slate-400 pb-2 font-montserrat">
+            Paste the URL to be shortened here:
+          </p>
+        </div>
+        <div className="mx-auto max-w-xl px-4 pb-4">
+          <form
+            method="post"
+            className="flex flex-1 justify-center align-middle"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className={isValid ? validInputForm : invalidInputForm}
+              placeholder="Enter link here"
+              onChange={(e) => {
+                setUrl(e.target.value);
+              }}
+            ></input>
+            <button
+              className="border-2
             cursor-pointer 
             bg-slate-600
             text-sm
@@ -82,44 +84,37 @@ export default function Home() {
             font-montserrat
             text-center
             w-1/6
-          hover:text-slate-600
-          min-w-fit"
-            type="submit"
-          >
-            Shorten
-          </button>
-        </form>
-        {!isValid ? (
-          <p className="text-center font-montserrat text-sm text-pink-600 mt-2">
-            Invalid url
-          </p>
-        ) : (
-          ""
-        )}
-        {/* <p className="text-center font-sans text-sm mt-2">
-          URLShortener is a free tool to shorten URLs and generate short links
-        </p>
-        <p className="text-center font-sans text-sm">
-          URLShortener allows to create a shortened link making it easy to share
-        </p> */}
-      </div>
-      {shortenedUrl != "" ? (
-        <div className="mx-auto max-w-xl px-4 pb-7">
-          <div className="mx-auto">
-            <p className="text-center pb-2 text-slate-400 font-montserrat">
-              Your shortened URL is:
+            min-w-[82px]
+          hover:text-slate-600"
+              type="submit"
+            >
+              Shorten
+            </button>
+          </form>
+          {!isValid ? (
+            <p className="text-center font-montserrat text-sm text-pink-600 mt-2">
+              Invalid url
             </p>
-          </div>
-          <div className="flex flex-1 justify-center align-middle">
-            <input
-              // className="border-2 border-slate-300 rounded-l-md ml-auto mr-auto p-3 w-5/6 hover:border-slate-500 focus:outline-none bg-gray-400"
-              className="border-2 border-slate-300 rounded-l-md p-3 hover:border-slate-500 flex-auto focus:outline-none bg-gray-400"
-              type="text"
-              value={shortenedUrl}
-              readOnly
-            />
-            <button
-              className="border-2
+          ) : (
+            ""
+          )}
+        </div>
+        {shortenedUrl != "" ? (
+          <div className="mx-auto max-w-xl px-4 pb-7">
+            <div className="mx-auto">
+              <p className="text-center pb-2 text-slate-400 font-montserrat">
+                Your shortened URL is:
+              </p>
+            </div>
+            <div className="flex flex-1 justify-center align-middle">
+              <input
+                className="border-2 border-slate-300 rounded-l-md p-3 hover:border-slate-500 flex-auto focus:outline-none bg-gray-400"
+                type="text"
+                value={shortenedUrl}
+                readOnly
+              />
+              <button
+                className="border-2
               cursor-pointer 
               bg-slate-500
               text-sm
@@ -129,17 +124,27 @@ export default function Home() {
               font-montserrat
               text-center
               w-1/6
-              min-w-fit
+            min-w-[82px]
             hover:text-slate-600"
-              onClick={copyUrl}
-            >
-              Copy
-            </button>
+                onClick={copyUrl}
+              >
+                Copy
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        ""
-      )}
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="mx-auto flex justify-center items-center h-96">
+        <p className="font-montserrat">
+          Want to create your own link?{" "}
+          <Link className="text-blue-400 hover:text-blue-600" href="/login">
+            Sign up
+          </Link>{" "}
+          now!{" "}
+        </p>
+      </div>
     </div>
   );
 }
